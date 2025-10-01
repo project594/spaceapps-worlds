613a58ae040b1927f016c4583af6b6d8ec1f0652
YUBO Charmer <homerorh090208@gmail.com>
2025-10-01 10:33:17 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,10 @@
+print("loading libraries...")
 import torch
 import torch.nn as nn
 import pandas as pd
 import joblib
 
-# 1. Recreate model architecture
+
 model = nn.Sequential(
     nn.Linear(38, 64),
     nn.ReLU(),
@@ -14,16 +15,15 @@ model = nn.Sequential(
     nn.Linear(32, 1)
 )
 
-# 2. Load trained weights
+print("loading model...")
 model.load_state_dict(torch.load("model.pth"))
 model.eval()
 
-# 3. Load preprocessing
 scaler = joblib.load("scaler.pkl")
 norm = torch.load("norm.pth")
 X_mean, X_std = norm["mean"], norm["std"]
 
-# 4. Load new data
+
 df_new = pd.read_csv("eval.csv")
 df_new = df_new.dropna()
 df_new = df_new.apply(pd.to_numeric, errors="coerce").astype("float64")
@@ -35,7 +35,7 @@ X_new = torch.tensor(X_new, dtype=torch.float32)
 # Standardize with training mean/std
 #X_new = (X_new - X_mean) / X_std
 
-# 5. Predict
+print("evaluating...")
 with torch.no_grad():
     logits = model(X_new)
     probs = torch.sigmoid(logits)   # convert logits → probabilities
